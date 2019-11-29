5256484add1abb73e2bf440a5ce22334f9871327
Phil de Joux <philderbeast@gmail.com>
2019-11-29 08:08:16 -0500

    Score the first 3 tasks of italy 2019 on the sphere.

---
+++ ft-score.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+
+COMP="--file=italy2019"
+
+fs-filter "$COMP"
+fs-route "$COMP"
+fs-effort "$COMP"
+fs-score "$COMP"
+
+extract-input "$COMP" --give-fraction=0.0001 --give-distance=5 --haversines
+task-length "$COMP"
+cross-zone "$COMP"
+tag-zone "$COMP"
+peg-frame "$COMP"
+align-time "$COMP"
+discard-further "$COMP"
+mask-track "$COMP"
+land-out "$COMP"
+gap-point "$COMP"
+
+comp-serve "$COMP"
