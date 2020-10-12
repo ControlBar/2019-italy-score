b6ffec41e353f0d90e036a2bea04308b91d861d8
Phil de Joux <philderbeast@gmail.com>
2020-10-12 06:06:43 -0400

    Run align-time on task 3 and then run subsequent commands.

--- ft-score.sh
+++ ft-score.sh
@@ -2,19 +2,19 @@
 
 COMP="--file=italy2019"
 
-fs-filter "$COMP"
-fs-route "$COMP"
-fs-arrival "$COMP"
-fs-effort "$COMP"
-fs-score "$COMP"
+#fs-filter "$COMP"
+#fs-route "$COMP"
+#fs-arrival "$COMP"
+#fs-effort "$COMP"
+#fs-score "$COMP"
 
-ft-extract-input "$COMP" --give-fraction=0.0001 --give-distance=5 --haversines
-ft-task-length "$COMP"
-ft-cross-zone "$COMP"
-ft-tag-zone "$COMP"
-ft-unpack-track "$COMP"
-ft-peg-frame "$COMP"
-ft-align-time "$COMP"
+#ft-extract-input "$COMP" --give-fraction=0.0001 --give-distance=5 --haversines
+#ft-task-length "$COMP"
+#ft-cross-zone "$COMP"
+#ft-tag-zone "$COMP"
+#ft-unpack-track "$COMP"
+#ft-peg-frame "$COMP"
+#ft-align-time "$COMP"
 ft-discard-further "$COMP"
 ft-area-step "$COMP"
 ft-mask-track "$COMP"
