977d13380c16f2efae953c7c5e3be5b1d257f788
Phil de Joux <philderbeast@gmail.com>
2020-11-16 11:29:25 -0500

    Replace mask-track with mask-* commands.

--- ft-score.sh
+++ ft-score.sh
@@ -2,22 +2,29 @@
 
 COMP="--file=italy2019"
 
-#fs-filter "$COMP"
-#fs-route "$COMP"
-#fs-arrival "$COMP"
-#fs-effort "$COMP"
-#fs-score "$COMP"
+fs-filter "$COMP"
+fs-route "$COMP"
+fs-arrival "$COMP"
+fs-effort "$COMP"
+fs-score "$COMP"
 
-#ft-extract-input "$COMP" --give-fraction=0.0001 --give-distance=5 --haversines
-#ft-task-length "$COMP"
-#ft-cross-zone "$COMP"
-#ft-tag-zone "$COMP"
-#ft-unpack-track "$COMP"
-#ft-peg-frame "$COMP"
-#ft-align-time "$COMP"
+ ft-extract-input "$COMP" --give-fraction=0.0001 --give-distance=5 --haversines
+ft-task-length "$COMP"
+ft-cross-zone "$COMP"
+ft-tag-zone "$COMP"
+ft-unpack-track "$COMP"
+ft-peg-frame "$COMP"
+
+ft-align-time "$COMP"
 ft-discard-further "$COMP"
 ft-area-step "$COMP"
-ft-mask-track "$COMP"
+
+ft-mask-arrival "$COMP"
+ft-mask-bonus "$COMP"
+ft-mask-effort "$COMP"
+ft-mask-lead "$COMP"
+ft-mask-reach "$COMP"
+
 ft-land-out "$COMP"
 ft-gap-point "$COMP"
 
