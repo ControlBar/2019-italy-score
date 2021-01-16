a0962cfa9c8e67fe4ecfd8159e7054a31ded09af
Phil de Joux <philderbeast@gmail.com>
2021-01-16 09:16:11 -0500

    Work in progress out of order tasks.

--- ft-score.sh
+++ ft-score.sh
@@ -2,31 +2,32 @@
 
 COMP="--file=italy2019"
 
-fs-clean "$COMP"
-fs-trim "$COMP"
-fs-route "$COMP"
-fs-arrival "$COMP"
-fs-effort "$COMP"
-fs-score "$COMP"
+# fs-clean "$COMP"
+# fs-trim "$COMP"
+# fs-route "$COMP"
+# fs-arrival "$COMP"
+# fs-effort "$COMP"
+# fs-score "$COMP"
+# 
+# ft-extract-input "$COMP" --give-fraction=0.001 --give-distance=5 --haversines
+# ft-task-length "$COMP"
+# ft-fly-time "$COMP"
+# ft-cross-zone "$COMP"
+# ft-tag-zone "$COMP"
 
-ft-extract-input "$COMP" --give-fraction=0.001 --give-distance=5 --haversines
-ft-task-length "$COMP"
-ft-fly-time "$COMP"
-ft-cross-zone "$COMP"
-ft-tag-zone "$COMP"
-
-ft-unpack-track "$COMP"
-ft-peg-frame "$COMP"
-
-ft-align-time "$COMP"
+# ft-unpack-track "$COMP"
+# ft-peg-frame "$COMP"
+# 
+# ft-align-time "$COMP"
 ft-discard-further "$COMP"
+ft-discard-further-stop "$COMP"
 ft-lead-area "$COMP"
 
 ft-mask-arrival "$COMP"
-ft-mask-bonus "$COMP"
 ft-mask-effort "$COMP"
 ft-mask-lead "$COMP"
 ft-mask-reach "$COMP"
+ft-mask-reach-stop "$COMP"
 
 ft-land-out "$COMP"
 ft-far-out "$COMP"
